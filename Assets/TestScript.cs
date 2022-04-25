b5393c92b92ced6f96de04f698ebd4e25100fada
ulasdgndnz <ulasdogandeniz@gmail.com>
2022-04-25 11:22:37 +0300

    Inital commit

---
+++ Assets/TestScript.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        print("Hello World!");
+    }
+}
