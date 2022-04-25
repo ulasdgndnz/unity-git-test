2f1d40f07d317fcb26a74733ed17855ea76a6f7f
Ulaş Doğan Deniz <ulasdogandeniz@gmail.com>
2022-04-25 14:47:28 +0300

    fvggtrhth

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -8,6 +8,11 @@ public class TestScript : MonoBehaviour
     private void Awake()
     {
         print("Hello World!");
+        print("Ben Berke!");
+        print("Ben Ulaş!");
+        print("Cümleten Sa");
+        print("Cümleten kAs");
+        
 
     }
 }
