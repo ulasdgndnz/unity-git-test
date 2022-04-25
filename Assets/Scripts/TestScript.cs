a54005be3c3c72b78406c1d12bbc8636106f1d2c
Ulaş Doğan Deniz <ulasdogandeniz@gmail.com>
2022-04-25 14:37:16 +0300

    dddd
    dddd

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        print("Hello World!");
+        print("Ben Berke!");
+        print("Ben Ulaş!");
+
+    }
+}
